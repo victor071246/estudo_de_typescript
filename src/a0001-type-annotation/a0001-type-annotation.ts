d895afa1efe0231de9f4e798676a06ee8c1664e1
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-07-23 23:31:19 -0300

    Estudando o tipo any em TypeScript

--- src/a0001-type-annotation/a0001-type-annotation.ts
+++ src/a0001-type-annotation/a0001-type-annotation.ts
@@ -1,1 +1,24 @@
+// Tipos básicos (aqui ocorre inferência de tipos)
 let nome: string = 'Luiz';
+let idade: number = 30;
+let adulto: boolean = true;
+let simbolo: symbol = Symbol('qualquer-symbol');
+// let big: bigint = 10n; //bigint
+
+//Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+let arrayDeNumeros2: number[] = [1, 2, 3];
+let arrayDeStrings: Array<string> = ['a', 'b'];
+let arrayDeStrings2: string[] = ['a', 'b'];
+
+// Objetos
+let pessoa: { nome: string; idade: number; adulto?: boolean } = {
+    idade: 30,
+    nome: 'Luiz',
+};
+
+//Funções
+function soma(x: number, y: number): number {
+    return x + y;
+}
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
