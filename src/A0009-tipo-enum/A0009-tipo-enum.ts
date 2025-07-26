65f9131c5ccb31d357f7a7c82db164a33bec542d
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-07-25 23:11:30 -0300

    Tipo unknown em TypeScript

---
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,23 @@
+enum Cores {
+    VERMELHO = 10, // 10
+    AZUL = 100, // 100
+    AMARELO = 200, // 200
+}
+
+enum Cores {
+    ROXO = 'ROXO',
+    VERDE = 201,
+    ROSA,
+}
+
+// console.log(Cores);
+// console.log(Cores.VERMELHO);
+// console.log(Cores[10]);
+// console.log(Cores.ROXO);
+// console.log(Cores);
+
+function escolhaACor(cor: Cores): void {
+    console.log(Cores[cor]);
+}
+
+// escolhaACor(123456);
