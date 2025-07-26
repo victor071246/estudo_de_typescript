2ac3bf0a30f6aa37ba2014ea1c73cd2389019c7a
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-07-25 23:37:16 -0300

    Tipos literais

--- src/A0009-tipo-enum/A0009-tipo-enum.ts
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -16,7 +16,7 @@ enum Cores {
 // console.log(Cores.ROXO);
 // console.log(Cores);
 
-function escolhaACor(cor: Cores): void {
+export function escolhaACor(cor: Cores): void {
     console.log(Cores[cor]);
 }
 
