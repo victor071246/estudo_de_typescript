6620f4c87e8bc1d24048601a591e56b28b498494
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-08-01 23:54:00 -0300

    Abstract methods e Abstract Class

---
+++ src/A0029-abstract-/A0029-abstract.ts
@@ -0,0 +1,43 @@
+export abstract class Personagem {
+    protected abstract emoji: string;
+
+    constructor(
+        protected nome: string,
+        protected ataque: number,
+        protected vida: number,
+    ) {}
+
+    atacar(personagem: Personagem): void {
+        this.bordao();
+        personagem.perderVida(this.ataque);
+    }
+
+    perderVida(forcaAtaque: number): void {
+        this.vida -= forcaAtaque;
+        console.log(`${this.emoji} - ${this.nome} agora tem ${this.vida} de vida...`);
+    }
+
+    abstract bordao(): void;
+}
+export class Guerreira extends Personagem {
+    protected emoji = '\u{1F9DD}';
+
+    bordao(): void {
+        console.log(this.emoji + 'GUERREIRA ESTÁ ATACANDO!');
+    }
+}
+export class Monstro extends Personagem {
+    protected emoji = '\u{1F9DF}';
+
+    bordao(): void {
+        console.log(this.emoji + 'MONSTRO ESTÁ ATACANDO!');
+    }
+}
+
+const guerreira = new Guerreira('Guerreira', 100, 1000);
+const monstro = new Monstro('Monstro', 87, 1000);
+
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+guerreira.atacar(monstro);
+monstro.atacar(guerreira);
