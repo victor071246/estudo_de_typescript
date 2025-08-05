fe3ae09bb751116f194a66da3035593910567823
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-08-04 23:21:40 -0300

    Interfaces e suas diferenças/semelhanças com TypeAlias

---
+++ src/A0034-interfaces/A0034-interfaces.ts
@@ -0,0 +1,36 @@
+interface TipoNome {
+    nome: string;
+}
+
+interface TipoSobrenome {
+    sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+    nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+    ) {}
+    nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+}
+
+const pessoaObj: TipoPessoa2 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    nomeCompleto() {
+        return this.nome + ' ' + this.sobrenome;
+    },
+};
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
