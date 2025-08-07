41a2e81a0b001b867497a76aed99ba407aac0e63
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-08-06 23:44:21 -0300

    Se o this já é difícil em JS imagina o this polimórfico

---
+++ src/A0040-this-polimorfico/A0040-this-polimorfico.ts
@@ -0,0 +1,49 @@
+export class Calculadora {
+    constructor(public numero: number) {}
+
+    add(n: number): this {
+        this.numero += n;
+        return this;
+    }
+
+    sub(n: number): this {
+        this.numero -= n;
+        return this;
+    }
+
+    div(n: number): this {
+        this.numero /= n;
+        return this;
+    }
+
+    mul(n: number): this {
+        this.numero *= n;
+        return this;
+    }
+}
+
+export class SubCalculadora extends Calculadora {
+    pow(n: number): this {
+        this.numero **= n;
+        return this;
+    }
+}
+
+const calculadora = new SubCalculadora(10);
+calculadora.add(5).mul(2).div(2).sub(5).pow(2);
+console.log(calculadora);
+
+export class RequestBuilder {
+    private method: 'get' | 'post' | null = null;
+    private url: string | null = null;
+
+    setMethod(method: 'get' | 'post'): this {
+        this.method = method;
+        return this;
+    }
+
+    setUrl(url: 'get' | 'post'): this {
+        this.url = url;
+        return this;
+    }
+}
