cccf8abb2543f6bcabfe02dd6b4d09ee7df7b90f
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-08-13 23:51:28 -0300

    Aula 4 sobre decorators em TS

---
+++ src/A0054-decorator-class-4/A0054-decorator-class-4.ts
@@ -0,0 +1,44 @@
+interface Constructor {
+    new (...args: any[]): any;
+}
+
+function inverteNomeECor(param1: string, param2: string) {
+    return function (target: Constructor) {
+        console.log('Sou o decorador e recebi ', target);
+
+        return class extends target {
+            cor: string;
+            nome: string;
+
+            constructor(...args: any[]) {
+                super(...args);
+                this.nome = this.inverte(args[0]);
+                this.cor = this.inverte(args[1]);
+            }
+
+            inverte(valor: string): string {
+                return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
+            }
+        };
+    };
+}
+function outroDecorador(param1: string) {
+    return function (target: Constructor) {
+        console.log('Sou o outro decorador' + param1);
+        return target;
+    };
+}
+
+@outroDecorador('Parâmetro do outro decorador') // 2
+@inverteNomeECor('Valor1', 'Valor2') // 1
+export class Animal {
+    constructor(
+        public name: string,
+        public cor: string,
+    ) {
+        console.log('Sou a classe');
+    }
+}
+
+const animal = new Animal('Luiz', 'roxo');
+console.log(animal);
