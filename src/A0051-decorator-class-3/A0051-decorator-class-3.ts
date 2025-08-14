0a513be5e26dd11734041b4e4df7f7504ec63ab0
Victor Felipe Dadério <daderiovictorfelipe@gmail.com>
2025-08-13 23:44:12 -0300

    Aula 3 sobre decorators em TS

--- src/A0051-decorator-class-3/A0051-decorator-class-3.ts
+++ src/A0051-decorator-class-3/A0051-decorator-class-3.ts
@@ -1,21 +1,23 @@
-function inverteNomeECor<T extends new (...args: any[]) => any>(target: T) {
-    return class extends target {
-        cor: string;
-        nome: string;
+function inverteNomeECor(param1: string, param2: string) {
+    return function <T extends new (...args: any[]) => any>(target: T) {
+        return class extends target {
+            cor: string;
+            nome: string;
 
-        constructor(...args: any[]) {
-            super(...args);
-            this.nome = this.inverte(args[0]);
-            this.cor = this.inverte(args[1]);
-        }
+            constructor(...args: any[]) {
+                super(...args);
+                this.nome = this.inverte(args[0]);
+                this.cor = this.inverte(args[1]);
+            }
 
-        inverte(valor: string): string {
-            return valor.split('').reverse().join('');
-        }
+            inverte(valor: string): string {
+                return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
+            }
+        };
     };
 }
 
-@inverteNomeECor
+@inverteNomeECor('Valor1', 'Valor2')
 export class Animal {
     constructor(
         public name: string,
